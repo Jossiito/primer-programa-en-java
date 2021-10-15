d2d202640abd761ea8ec9f1af684ab0df09097a4
Jose <josito.scb11@gmail.com>
2021-10-15 09:50:46 +0200

    Agregado cantante favorito

--- Prueba.java
+++ Prueba.java
@@ -2,9 +2,9 @@ public class Prueba {
 	
 	public static void main(String args[]) {
 	
-		System.out.println("Jose Manuel Alonso Pozas");
 		System.out.println("León 2001");
 		System.out.println("León Windows 10");
+		System.out.println("Mi cantante favorito es Ana Mena");
 	}
 }
 
